575c6ae71812eaef5d82d8dec8d1aea03dd2e180
Pierre Dahmani <hi@pierre-dev.com>
2023-05-16 22:15:45 +0200

    mini mini eslint prettier typescript "NO recommended eslint rules" example setup.

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    // NOTE: Here I simply want to disable all rules and enable them one by one
+    // later. In case you want all "recommended" rules, simply uncomment here
+    // and you are ready to go.
+    // "eslint:recommended",
+    // "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  // This enables type checking for typescript files. Here we can simpl add each
+  // eslint or typescript-eslint rule. Src for the overrides from here:
+  // src: https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/eslint-recommended.ts
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.mts", "*.cts"],
+      rules: {
+        // sample typescript-eslint rule
+        // https://typescript-eslint.io/rules/array-type
+        "@typescript-eslint/array-type": "error",
+        // sample eslint rule
+        // src: https://eslint.org/docs/latest/rules/eqeqeq
+        eqeqeq: ["error"],
+      },
+    },
+  ],
+  root: true,
+};
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const x: number[] = [1, 2, 3];
+const y: Array<number> = [1, 2, 3];
+
+if (x == y) {
+  console.log("equal!");
+}
+
+console.log("hello world");
